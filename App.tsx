9e6b3501d0e699eef962ba4d83aeccf18d5a265c
Carlos Eduardo Nakashoji <eduardo.nakashoji@gmail.com>
2022-04-05 10:27:15 -0300

    Instalação das libs

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { useFonts } from 'expo-font';
 import React from 'react';
+import * as Linking from 'expo-linking';
 import {
 	OpenSans_400Regular,
 	OpenSans_600SemiBold,
@@ -10,12 +11,18 @@ import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/styles/theme';
 
+const prefix = Linking.createURL('/');
+
 export default function App() {
 	const [fontsLoaded] = useFonts({
 		OpenSans_400Regular,
 		OpenSans_600SemiBold,
 		OpenSans_700Bold,
 	});
+
+	const linking = {
+		prefixes: [prefix],
+	};
 
 	if (!fontsLoaded) {
 		return <AppLoading />;
